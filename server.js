d6757a0421420e104b8c8eff83e127e34ca77285
Deepinder Badesha <dbadesha@salesforce.com>
2016-09-06 17:06:20 -0700

    setup for heroku deployment

--- server.js
+++ server.js
@@ -2,7 +2,17 @@ var express = require('express');
 
 // Create our app
 var app = express();
+//for heroku to provide port or use local
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next){
+  if(req.headers['x-forwarded-proto'] == 'http'){
+    next();
+  } else {
+    res.redirect('http://'+ req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log('server is up and running on port 3000');
 });
