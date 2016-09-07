a696066f6bdf7efd922e649a9d09de1f9c9143ad
Deepinder Badesha <dbadesha@salesforce.com>
2016-09-07 12:20:13 -0700

    about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,25 @@ var React = require('react');
 var About = () =>{
   return(
   <div>
-    <h3>About me</h3>
-    <p>This is About me page, Welcome to React Weather App.</p>
+    <h1 className="text-center">About Weather App</h1>
+    <p>React Weather App allows users to search weather by City Name, Currently it returns weather in cellis degree, This App is developed using following technologies</p>
+    <ol>
+      <li><a href="https://facebook.github.io/react/">React</a> Javascript Framework</li>
+      <li><a href="https://github.com/reactjs/react-router">Router</a> Routing for React Components</li>
+      <li><a href="https://facebook.github.io/react/docs/jsx-in-depth.html">JSX</a> JavaScript Syntax Ext</li>
+      <li><a href="http://foundation.zurb.com/sites/docs/">Styles</a> Foundation Doc</li>
+      <li><a href="http://openweathermap.org/api">API Doc</a> Open Weather</li>
+    </ol>
+    <p>
+      In Future it will have more features related to weather
+    </p>
+    <ol>
+      <li>Forecast for 1-10 days</li>
+      <li>Heat Map</li>
+      <li>Weather Related News</li>
+      <li>Integration with AI</li>
+      <li>Alerts Notification</li>
+    </ol>
   </div>
   )
 };
