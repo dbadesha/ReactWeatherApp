6eb97c42f80ae1f0d7936360a88f1d6caa32c040
Deepinder Badesha <dbadesha@salesforce.com>
2016-09-07 11:47:59 -0700

    example page styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,9 +1,17 @@
 var React = require('react');
+var {Link}= require('react-router');
+
 var Examples = ()=> {
   return(
   <div>
-    <h3>Examples</h3>
-    <p>This is example page, Will list out stuff shortly.</p>
+    <h1 className="text-center">Examples</h1>
+    <p>Following are few example city for weather serach</p>
+    <ul>
+      <li><Link to ='/?location=San Franscisco'> San Francisco, CA</Link></li>
+      <li><Link to ='/?location=Austin'> Austin, TX</Link></li>
+      <li><Link to ='/?location=Delhi'>Delhi, India</Link></li>
+      <li><Link to ='/?location=frankfurt'>Frankfurt Germany</Link></li>
+    </ul>
   </div>
   )
 };
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -4,9 +4,12 @@ var Nav = require('Nav');
 var Main = (props) =>{
   return(
     <div>
-      <h1>Main Component</h1>
       <Nav/>
+      <div className="row">
+        <div className ="columns medium-6 large-4 small-centered">
       {props.children}
+        </div>
+      </div>
     </div>
     );
 }
