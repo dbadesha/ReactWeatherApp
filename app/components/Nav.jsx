6466022d8ae0107f6a5bb7daff7dc7a6cf1f4f6e
Deepinder Badesha <dbadesha@salesforce.com>
2016-09-07 11:06:47 -0700

    nav styles

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,16 +1,43 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = () =>{
-  return(
-    <div>
-      <h2> From Nav Component</h2>
-      <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink><br/>
-      <Link to='/about' activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link><br/>
-      <Link to='/examples' activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link><br/><br/>
-      <a href='#/about'>CheckItOut!</a>
+var Nav = React.createClass({
+  onSerach: function(e){
+    e.preventDefault();
+    alert('Not wired yet');
+  },
+  render: function(){
+    return(
+    <div className = "top-bar">
+      <div className ="top-bar-left">
+        <ul className ="menu">
+          <li className ="menu-text"> React Weather App</li>
+          <li >
+            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+          </li>
+          <li>
+            <Link to='/about' activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
+          </li>
+          <li>
+            <Link to='/examples' activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+          </li>
+        </ul>
+      </div>
+      <div className ="top-bar-right">
+        <form onSubmit={this.onSerach}>
+          <ul className="menu">
+            <li>
+              <input type= "serach" placeholder="Serach City Name"/>
+            </li>
+            <li>
+              <input type="submit" className ="button" value="Get Weather"/>
+            </li>
+          </ul>
+        </form>
+      </div>
     </div>
-  );
-}
+    );
+  }
+});
 
 module.exports = Nav;
